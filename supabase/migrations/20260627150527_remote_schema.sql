b3abed4b9903ee97dc6914a5df8b947c6ea6f44e
juanlinuz <johndev1898@outlook.com>
2026-06-27 23:52:41 +0800

    feat: add permissions for document, library, and user tables

---
+++ supabase/migrations/20260627150527_remote_schema.sql
@@ -0,0 +1,145 @@
+grant delete on table "public"."document_chunks" to "anon";
+
+grant insert on table "public"."document_chunks" to "anon";
+
+grant select on table "public"."document_chunks" to "anon";
+
+grant update on table "public"."document_chunks" to "anon";
+
+grant delete on table "public"."document_chunks" to "authenticated";
+
+grant insert on table "public"."document_chunks" to "authenticated";
+
+grant select on table "public"."document_chunks" to "authenticated";
+
+grant update on table "public"."document_chunks" to "authenticated";
+
+grant delete on table "public"."document_chunks" to "service_role";
+
+grant insert on table "public"."document_chunks" to "service_role";
+
+grant select on table "public"."document_chunks" to "service_role";
+
+grant update on table "public"."document_chunks" to "service_role";
+
+grant delete on table "public"."document_edits" to "anon";
+
+grant insert on table "public"."document_edits" to "anon";
+
+grant select on table "public"."document_edits" to "anon";
+
+grant update on table "public"."document_edits" to "anon";
+
+grant delete on table "public"."document_edits" to "authenticated";
+
+grant insert on table "public"."document_edits" to "authenticated";
+
+grant select on table "public"."document_edits" to "authenticated";
+
+grant update on table "public"."document_edits" to "authenticated";
+
+grant delete on table "public"."document_edits" to "service_role";
+
+grant insert on table "public"."document_edits" to "service_role";
+
+grant select on table "public"."document_edits" to "service_role";
+
+grant update on table "public"."document_edits" to "service_role";
+
+grant delete on table "public"."documents" to "anon";
+
+grant insert on table "public"."documents" to "anon";
+
+grant select on table "public"."documents" to "anon";
+
+grant update on table "public"."documents" to "anon";
+
+grant delete on table "public"."documents" to "authenticated";
+
+grant insert on table "public"."documents" to "authenticated";
+
+grant select on table "public"."documents" to "authenticated";
+
+grant update on table "public"."documents" to "authenticated";
+
+grant delete on table "public"."documents" to "service_role";
+
+grant insert on table "public"."documents" to "service_role";
+
+grant select on table "public"."documents" to "service_role";
+
+grant update on table "public"."documents" to "service_role";
+
+grant delete on table "public"."libraries" to "anon";
+
+grant insert on table "public"."libraries" to "anon";
+
+grant select on table "public"."libraries" to "anon";
+
+grant update on table "public"."libraries" to "anon";
+
+grant delete on table "public"."libraries" to "authenticated";
+
+grant insert on table "public"."libraries" to "authenticated";
+
+grant select on table "public"."libraries" to "authenticated";
+
+grant update on table "public"."libraries" to "authenticated";
+
+grant delete on table "public"."libraries" to "service_role";
+
+grant insert on table "public"."libraries" to "service_role";
+
+grant select on table "public"."libraries" to "service_role";
+
+grant update on table "public"."libraries" to "service_role";
+
+grant delete on table "public"."library_members" to "anon";
+
+grant insert on table "public"."library_members" to "anon";
+
+grant select on table "public"."library_members" to "anon";
+
+grant update on table "public"."library_members" to "anon";
+
+grant delete on table "public"."library_members" to "authenticated";
+
+grant insert on table "public"."library_members" to "authenticated";
+
+grant select on table "public"."library_members" to "authenticated";
+
+grant update on table "public"."library_members" to "authenticated";
+
+grant delete on table "public"."library_members" to "service_role";
+
+grant insert on table "public"."library_members" to "service_role";
+
+grant select on table "public"."library_members" to "service_role";
+
+grant update on table "public"."library_members" to "service_role";
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
